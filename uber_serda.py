66a373077371cd3366b8c46f904497d5ce87215f
Bo Molenaar <bo.molenaar@ru.nl>
2023-03-22 15:48:45 +0100

    improved arg handling and now asks for project dir

--- uber_serda.py
+++ uber_serda.py
@@ -1,44 +1,75 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import argparse
+import shutil
 import serda_data_sel as data_sel
 import serda_data_prep as data_prep
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--clean', help = "Flag specifying whether you want to generate new directories. Default=False", action = 'store_true', required=False)
-parser.add_argument('-a', '--audiozip', required='--clean' in sys.argv, help = "Path to raw audio zip. Required when using --clean.")
-parser.add_argument('-l', '--logzip', required='--clean' in sys.argv, help = "Path to raw log zip. Required when using --clean")
-parser.add_argument('audio_path', help = "Path to audio processing and storing dir")
-parser.add_argument('log_path', help = "Path to log processing and storing dir")
-parser.add_argument('prompts_source', help = "Path to story prompts")
-parser.add_argument('prompt_path', help = "Path to prompt processing and storing dir")
-parser.add_argument('recs_to_ignore', help = "Location of a file specifying recordings to ignore")
-parser.add_argument('stories_AO_source', help = "Path to ASR output for story task recs")
-# parser.add_argument('words_AO_source', help = "Path to ASR output for word task recs")
+parser.add_argument('--clean', action = 'store_true', required=False,
+                    help = "Flag specifying whether you want to generate new directories."
+                    " When True, script will delete everything under project_dir"
+                    " and generate new directories and files starting from audio zip and logs zip."
+                    " When False, will leave files as is"
+                    " and only collect their paths for data preparation steps."
+                    " Default=False")
+parser.add_argument('-a', '--audiozip', required='--clean' in sys.argv,
+                    help = "Path to raw audio zip. Required when using --clean.")
+parser.add_argument('-l', '--logzip', required='--clean' in sys.argv,
+                    help = "Path to raw log zip. Required when using --clean.")
+parser.add_argument('project_dir',
+                    help = "Parent project directory where you want to process and store audio, logs, prompts and ASR transcriptions.")
+parser.add_argument('audio_dir',
+                    help = "Path to audio processing and storing dir. A subdirectory of project_dir."
+                    "Default = 'audio'", default = 'audio')
+parser.add_argument('log_dir',
+                    help = "Path to log processing and storing dir. A subdirectory of project_dir."
+                    "Default = 'logs'", default = 'logs')
+parser.add_argument('prompt_dir',
+                    help = "Path to prompt processing and storing dir. A subdirectory of project_dir."
+                    "Default = 'prompts'", default= 'prompts')
+parser.add_argument('prompts_source',
+                    help = "Path to story prompts."
+                    " Expected prompt files are 1 sentence per line and filenames end with _clean.txt")
+parser.add_argument('recs_to_ignore',
+                    help = "Location of a .txt file specifying recordings to ignore."
+                    " Each line should contain the ID of one recording (! not a path)."
+                    " E.g. AB123-story_1-20230101090012345")
 
 args = parser.parse_args()
-print(vars(args))
-
-
 if args.clean and (args.audiozip is None or args.logzip is None):
     parser.error("--clean requires --audiozip and --logzip.")
 
+audio_path = os.path.join(args.project_dir, args.audio_dir)
+logs_path = os.path.join(args.project_dir, args.log_dir)
+prompts_path = os.path.join(args.project_dir, args.prompt_dir)
+
 print("\n###\tSERDA v1 data processing\t###\n")
 
+for mydir in [args.project_dir, args.audio_dir, args.log_dir, args.prompt_dir]:
+    if args.clean and os.path.isdir(mydir):
+        print("Creating new project dir...")
+        shutil.rmtree(mydir)
+        os.mkdir(mydir)
+    
 print("\n# 1. Data selection  #\n")
 print("Creating dict of selected data...")
 if args.clean:
-    full_dict = data_sel.gen_clean_dict(args.audio_path, args.log_path, args.recs_to_ignore, args.clean, args.audio_zip, args.log_zip)
+    full_dict = data_sel.gen_clean_dict(audio_path, logs_path, args.recs_to_ignore, args.clean, args.audiozip, args.logzip)
 else:
-    full_dict = data_sel.gen_clean_dict(args.audio_path, args.log_path, args.recs_to_ignore, args.clean)
+    full_dict = data_sel.gen_clean_dict(audio_path, logs_path, args.recs_to_ignore, args.clean)
 print("Done.")
 
 print("\n# 2. Data preparation #\n")
 print("Segmenting data and matching prompts...")
-data_prep.prepare_data(args.clean, full_dict, args.audio_path, args.log_path, args.prompts_source, args.prompt_path, args.stories_AO_source)#, args.words_AO_source)
+data_prep.prepare_data(args.clean, full_dict, audio_path, logs_path, args.prompts_source, prompts_path)
 print("Done.")
 
 print("\n# Finished preparing data #\n")
+
+print("Please prepare a folder with ASR output for words and story tasks in"
+      f" {os.path.join(args.project_dir, 'asr')}, so you can run segment_stories.py")
